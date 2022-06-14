f16f32527da5eb60cd451995804c36a5781bcba7
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-14 22:39:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData (name, email) {
+    const usersInfo = {name: name, email: email};
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(usersInfo)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.innerHTML = data.id
+    })
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+
+}
